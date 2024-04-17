17b45009a37ea766f84979e887e2135bfd9a326b
Sushil <kidsushilink@gmail.com>
2024-04-16 23:07:27 -0700

    view.py update

--- jobportal/job/views.py
+++ jobportal/job/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -15,4 +17,23 @@ def recruiter_login(request):
     return render(request, 'recruiter_login.html')
 
 def user_signup(request):
-    return render(request, 'user_signup.html')
+    error = ""
+    if request.method=='POST':
+        f   = request.POST['fname']
+        l   = request.POST['lname']
+        i   = request.FILES['image']
+        p   = request.POST['pwd']
+        e   = request.POST['email']
+        con = request.POST['contact']
+        gen = request.POST['gender']
+
+        try:
+            user = User.objects.create_user(first_name=f, last_name=l, username=e, password=p)
+            StudentUser.objects.create(user=user, mobile=con, image=i, gender=gen, type="student")
+            error="no"
+        except:
+            error="yes"
+
+    d = {'error': error}
+
+    return render(request, 'user_signup.html', d)
